0ba98baa2335e4f7abdb6abf583f32c3f6497c6a
Mark Wilson <mwilson@rippleffect.com>
2014-07-08 13:25:31 +0100

    Adding comments

--- src/MarkWilson/XmlToJson/XmlToJsonConverter.php
+++ src/MarkWilson/XmlToJson/XmlToJsonConverter.php
@@ -2,17 +2,40 @@
 
 namespace MarkWilson\XmlToJson;
 
+/**
+ * XML to JSON string converter
+ *
+ * @package MarkWilson\XmlToJson
+ * @author  Mark Wilson <mark@89allport.co.uk>
+ */
 class XmlToJsonConverter
 {
+    /**
+     * Convert from SimpleXMLElement to json string
+     *
+     * @param \SimpleXMLElement $xml XML element
+     *
+     * @return string
+     */
     public function convert(\SimpleXMLElement $xml)
     {
         return json_encode([$xml->getName() => $this->getData($xml)]);
     }
 
+    /**
+     * Get the XML data
+     *
+     * Returns an array of the XML data. If the XML data is just a single value it will return a string instead.
+     *
+     * @param \SimpleXMLElement $xml XML element
+     *
+     * @return array|string
+     */
     private function getData(\SimpleXMLElement $xml)
     {
         $data = [];
 
+        // loop through the attributes and append them to the data array with '-' suffix on keys
         foreach ($xml->attributes() as $key => $value) {
             $data['-' . $key] = (string)$value;
         }
@@ -20,9 +43,11 @@ class XmlToJsonConverter
         if ($xml->count() > 0) {
             $children = $xml->children();
 
+            // loop through the children
             foreach ($children as $key => $child) {
                 $childData = $this->getData($child);
 
+                // decide how to put this into the data array, if the key exists it becomes an array of values
                 if (isset($data[$key])) {
                     if (is_array($data[$key])) {
                         $data[$key][] = $childData;
@@ -34,8 +59,10 @@ class XmlToJsonConverter
                 }
             }
         } else {
+            // get the string value of the XML
             $value = (string)$xml;
 
+            // check if this is just a single value element, i.e. <Element>Value</Element
             if (count($data) === 0) {
                 $data = $value;
             } else {
