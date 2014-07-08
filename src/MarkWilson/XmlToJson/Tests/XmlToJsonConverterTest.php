1e335ae5b4365c6a08efe3716ad84da2e5103300
Mark Wilson <mwilson@rippleffect.com>
2014-07-08 13:09:55 +0100

    Adding more tests

--- src/MarkWilson/XmlToJson/Tests/XmlToJsonConverterTest.php
+++ src/MarkWilson/XmlToJson/Tests/XmlToJsonConverterTest.php
@@ -4,14 +4,30 @@ namespace MarkWilson\XmlToJson\Tests;
 
 class XmlToJsonConverterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testConvert()
+    /**
+     * @dataProvider getConvertData
+     */
+    public function testConvert($xmlString, $expectedJson)
     {
-        $xml = new \SimpleXMLElement('<testing myattr="attribute"><test>Testing</test></testing>');
+        $xml = new \SimpleXMLElement($xmlString);
 
         $converter = new \MarkWilson\XmlToJson\XmlToJsonConverter();
         $json = $converter->convert($xml);
 
-        $expectedJson = '{"testing":{"-myattr":"attribute","test":{"#text":"Testing"}}}';
         $this->assertEquals($expectedJson, $json);
+    }
+
+    public function getConvertData()
+    {
+        return [
+            ['<testing myattr="attribute"><test>Testing</test></testing>', '{"testing":{"-myattr":"attribute","test":{"#text":"Testing"}}}'],
+            ['<Element>Value</Element>', '{"Element":"Value"}'],
+            ['<Element attribute="Attribute value">value</Element>', '{"Element":{"-attribute":"Attribute value","#text":"value"}}'],
+            ['<Element><FirstSubElement>1st value</FirstSubElement><SecondSubElement>2nd value</SecondSubElement></Element>', '{"Element":{"FirstSubElement":"1st value","SecondSubElement":"2nd value"}}'],
+            ['<Element><SubElement>1st</SubElement><SubElement>2nd</SubElement></Element>', '{"Element":{"SubElement":["1st","2nd"]}}'],
+            ['<Element>Test</Element>', '{"Element":"Test"}'],
+            ['<Element><Test /></Element>', '{"Element":"Test"}'],
+            ['<Element><SubElement>Value 1</SubElement><SubElement>Value 2</SubElement></Element>', '{"Element":{"SubElement":["Value 1","Value 2"]}}'],
+        ];
     }
 }
